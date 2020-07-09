e51e2ffe6b471dead9533da3e5ab6d440e4bbadd
ilham-mmr <54584081+ilham-mmr@users.noreply.github.com>
2020-07-09 17:07:39 +0800

    Create Animal.java

---
+++ Animal.java
@@ -0,0 +1,15 @@
+public class Animal{
+    private String name;
+    public static int age;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+    
+    public void introduceYourself(){
+        System.out.println("Morr. I am an animal.");
+  
